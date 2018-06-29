382387eaead1e792f72122eb34867894f4610ad2
talhanousher <talhanousher@gmail.com>
2018-06-30 00:19:21 +0500

    Error Handling and Callbacks

--- index.js
+++ index.js
@@ -7,16 +7,29 @@
 //   },
 // };
 
-var rectangle = require ('./rectangle');
-function solveRectangle (length, breadth) {
-  if (length > 0 && breadth > 0) {
-    console.log (
-      'Parameter is ' + rectangle.parameter (length, breadth) + ' unit'
-    );
-    console.log ('Area is ' + rectangle.area (length, breadth) + ' unit');
-  } else {
-    console.log ('Length and Breadth must be positive');
-  }
+var rectangle = require('./rectangle');
+function solveRectangle(length, breadth) {
+  // if (length > 0 && breadth > 0) {
+  //   console.log (
+  //     'Parameter is ' + rectangle.parameter (length, breadth) + ' unit'
+  //   );
+  //   console.log ('Area is ' + rectangle.area (length, breadth) + ' unit');
+  // } else {
+  //   console.log ('Length and Breadth must be positive');
+  // }
+  rectangle(length, breadth, (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Area : ' + res.area());
+      console.log('Parameter : ' + res.parameter());
+    }
+  });
 }
+console.log("saasdasd")
 
-solveRectangle (4, 3);
+solveRectangle(4, 3);
+solveRectangle(4, -3);
+solveRectangle(-4, -3);
+solveRectangle(-4, 3);
+
