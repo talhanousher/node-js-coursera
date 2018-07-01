25053402eff63515b1c87460ce1e4399d98f2051
talhanousher <talhanousher@gmail.com>
2018-07-01 16:41:47 +0500

    Using morgan nad express.static

--- node-express/index.js
+++ node-express/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var http = require('http');
+var morgan = require('morgan');
 var hostName = 'localhost';
 var port = 3000;
 var app = express();
+app.use(morgan('dev'));
 
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-    console.log(req.headers);
+    // console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><head></head><body><h1>Hello Express</h1></body></html>');
