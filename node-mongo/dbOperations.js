3f5bdad62408aed5af4d0233362b6d554ecb43f8
talhanousher <talhanousher@gmail.com>
2018-07-07 23:40:39 +0500

    Promise Based mongodb functions

--- node-mongo/dbOperations.js
+++ node-mongo/dbOperations.js
@@ -2,43 +2,37 @@ var assert = require('assert');
 
 exports.insertDocument = (db, document, collection, callback) => {
     var coll = db.collection(collection);
-    coll.insert(document, (err, res) => {
-        assert.equal(err, null);
-        console.log('Inserted : ' + res.result.n + " documents into collection : " + collection);
-        callback(res);
-    })
+    // coll.insert(document, (err, res) => {
+    //     assert.equal(err, null);
+    //     console.log('Inserted : ' + res.result.n + " documents into collection : " + collection);
+    //     callback(res);
+    // });
+    return coll.insert(document);
 };
-
-
 exports.findDocuments = (db, collection, callback) => {
     var coll = db.collection(collection);
-    coll.find({}).toArray((err, res) => {
-        assert.equal(err, null);
-        console.log('Found ' , res);
-        callback(res);
-    });
-
+    // coll.find({}).toArray((err, res) => {
+    //     assert.equal(err, null);
+    //     console.log('Found ' , res);
+    //     callback(res);
+    // });
+    return coll.find({}).toArray();
 };
-
-
-
 exports.removeDocument = (db, document, collection, callback) => {
     var coll = db.collection(collection);
-    collection.deleteOne(document, (err, res) => {
-        assert.equal(err, null);
-        console.log('Deleted One : ' + res);
-        callback(res)
-    })
-
+    // collection.deleteOne(document, (err, res) => {
+    //     assert.equal(err, null);
+    //     console.log('Deleted One : ' + res);
+    //     callback(res)
+    // });
+    return collection.deleteOne(document);
 };
-
-
 exports.updateDocument = (db, document, update, collection, callback) => {
     var coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, res) => {
-        assert.equal(err, null);
-        console.log('Updated : ' + res);
-        callback(res);
-    })
-
+    // coll.updateOne(document, { $set: update }, null, (err, res) => {
+    //     assert.equal(err, null);
+    //     console.log('Updated : ' + res);
+    //     callback(res);
+    // });
+    return coll.updateOne(document, { $set: update }, null);
 };
--- node-mongo/index.js
+++ node-mongo/index.js
@@ -3,41 +3,76 @@ var assert = require('assert');
 var url = 'mongodb://localhost:27017/conFusion';
 var dbName = 'conFusion';
 var dbOperation = require('./dbOperations');
-mongoClient.connect(url, (err, client) => {
-    assert.equal(err, null);
-    console.log('Connect To Db Successfully');
-    var db = client.db(dbName);
-    dbOperation.insertDocument(db, { "name": "testdish2", "description": "testdescription2" }, "dishes", (res) => {
-        console.log('Inserted : ', res.ops);
-        dbOperation.findDocuments(db, 'dishes', (res) => {
-            console.log('Found : ', res);
-            dbOperation.updateDocument(db, { "name": "testdish2" }, { "description": "updateddescription2" }, "dishes", (res) => {
-                console.log("Updated", res.result);
-                dbOperation.findDocuments(db, 'dishes', (res) => {
-                    console.log('Found Updated : ', res);
-                    db.dropCollection('dishes', (res)=>{
-                        console.log(res);
-                        client.close();
-                    } )
-                })
+mongoClient.connect(url)
+    .then((client) => {
+        console.log('Connected Successfully to the Server');
+        var db = client.db(dbName);
+        dbOperation.insertDocument(db, { "name": "testpromisedish1", "description": "testpromisedescription1" }, 'dishes')
+            .then((res) => {
+                console.log('Insert Document : ', res.ops);
+                return dbOperation.findDocuments(db, 'dishes');
             })
-        })
-    });
-    // var collection = db.collection('dishes');
-    // collection.insertOne({ "name": "testdish1", "description": "testdescription" }, (err, result) => {
-    //     assert.equal(err, null);
-    //     console.log('After Insertion');
-    //     console.log(result);
+            .then((res) => {
+                console.log('Found Document : ', res);
+                return dbOperation.updateDocument(db, { "name": "testpromisedish1" }, { "description": "updatedtestpromisedescription" }, 'dishes');
+            })
+            .then((res) => {
+                console.log('Updated Document ', res.result);
+                return dbOperation.findDocuments(db, 'dishes');
+            })
+            .then((res) => {
+                console.log('Found Updated Document : ', res);
+                return db.dropCollection('dishes');
+            })
+            .then((res) => {
+                console.log('Dropped Collection : ', res)
+                return client.close();
+            })
+            .catch((err) => {
+                console.log('Line 32', err)
+            })
+    })
+    .catch((err) => {
+        console.log('Line 37',err);
+    })
 
-    //     collection.find({}).toArray((err, docs) => {
-    //         assert.equal(err, null);
-    //         console.log('Found');
-    //         console.log(docs);
-    //     });
 
-    //     db.dropCollection('dishes', (err, result) => {
-    //         assert.equal(err,null);
-    //         client.close();
-    //     })
-    // });
-});
+
+
+
+// mongoClient.connect(url, (err, client) => {
+//     assert.equal(err, null);
+//     console.log('Connect To Db Successfully');
+//     var db = client.db(dbName);
+//     dbOperation.insertDocument(db, { "name": "testdish2", "description": "testdescription2" }, "dishes", (res) => {
+//         console.log('Inserted : ', res.ops);
+//         dbOperation.findDocuments(db, 'dishes', (res) => {
+//             console.log('Found : ', res);
+//             dbOperation.updateDocument(db, { "name": "testdish2" }, { "description": "updateddescription2" }, "dishes", (res) => {
+//                 console.log("Updated", res.result);
+//                 dbOperation.findDocuments(db, 'dishes', (res) => {
+//                     console.log('Found Updated : ', res);
+//                     db.dropCollection('dishes', (res)=>{
+//                         console.log(res);
+//                         client.close();
+//                     } )
+//                 })
+//             })
+//         })
+//     });
+//     // var collection = db.collection('dishes');
+//     // collection.insertOne({ "name": "testdish1", "description": "testdescription" }, (err, result) => {
+//     //     assert.equal(err, null);
+//     //     console.log('After Insertion');
+//     //     console.log(result);
+//     //     collection.find({}).toArray((err, docs) => {
+//     //         assert.equal(err, null);
+//     //         console.log('Found');
+//     //         console.log(docs);
+//     //     });
+//     //     db.dropCollection('dishes', (err, result) => {
+//     //         assert.equal(err,null);
+//     //         client.close();
+//     //     })
+//     // });
+// });
