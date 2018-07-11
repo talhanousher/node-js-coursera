3b3294985973c60e087792f28ffbefe9f6747237
talhanousher <talhanousher@gmail.com>
2018-07-12 01:09:41 +0500

    Assignment-2

---
+++ Assignment-2/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var http = require('http');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var promotionRouter = require('./routes/promotionsRoute');
+var leaderRouter = require('./routes/leaderRouter');
+var url = 'mongodb://localhost:27017';
+var connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log('Connect Succesfully to the Server');
+}, (err) => {
+    console.log('Error : ', err);
+})
+
+var port = 3000;
+var hostName = 'localhost';
+var app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/promotions', promotionRouter);
+app.use('/leaders', leaderRouter);
+
+var server = http.createServer(app);
+server.listen(port, hostName, () => {
+    console.log('Server is Running at https://' + hostName + ":" + port);
+});
---
+++ Assignment-2/routes/leaderRouter.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var leaderRouter = express.Router();
+leaderRouter.use(bodyParser.json());
+var Leaders = require('../model/leader');
+var mongoose = require('mongoose');
+
+leaderRouter.route('/')
+    .get((req, res) => {
+        Leaders.find({})
+            .then((data) => {
+                res.statusCode = 200,
+                    res.setHeader('Content-Type', 'application/json');
+                res.json(data);
+            })
+            .catch((err) => {
+                console.log('Error GET ', err);
+            })
+    })
+    .post((req, res) => {
+        console.log(req.body);
+        Leaders.create(req.body)
+            .then((leader) => {
+                console.log('Created Leaders : ', leader);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leader);
+            })
+            .catch((err) => {
+                console.log('Error POST : ', err);
+            })
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT not supported');
+    })
+    .delete((req, res, next) => {
+        Leaders.remove({})
+            .then((resp) => {
+                console.log(resp);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            })
+            .catch((err) => {
+                console.log('Error DELETE ', err);
+            })
+    });
+
+
+leaderRouter.route('/:leadersId')
+    .get((req, res) => {
+        Leaders.findById(req.params.leadersId)
+            .then((leader) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leader);
+            })
+            .catch((err) => {
+                console.log("Error GET:/ : ", err);
+            })
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end('Post Not supported on a specific promotion with id : ' + req.params.leadersId);
+    })
+    .put((req, res, next) => {
+        Leaders.findByIdAndUpdate(req.params.leadersId, {
+            $set: req.body
+        }, {
+                new: true
+            })
+            .then((leader) => {
+                console.log(leader);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leader);
+            })
+            .catch((err) => {
+                console.log('Error UPDATE://DISHID : ', err);
+            })
+    })
+    .delete((req, res, next) => {
+        // res.end('Deleting dish : ' + req.body.name);
+        Leaders.findByIdAndRemove(req.params.leadersId)
+            .then((resp) => {
+                console.log(resp);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            })
+            .catch((err) => {
+                console.log('Error DELETE ', err);
+            })
+    });
+
+
+
+module.exports = leaderRouter;
