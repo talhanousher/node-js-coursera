c5a6550d9277ce69793c3bc9c954ee9457f029a1
talhanousher <talhanousher@gmail.com>
2018-07-15 00:57:22 +0500

    Basic Authentication

--- Assignment-2/index.js
+++ Assignment-2/index.js
@@ -20,6 +20,35 @@ var hostName = 'localhost';
 var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+function auth(req, res, next) {
+    console.log(req.headers);
+
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('Not Authenticated');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
+
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+    if (username === 'admin' && password === 'password') {
+        next();
+    } else {
+        var err = new Error('Not Authenticated');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
+}
+app.use(auth);
+
+
+
 app.use('/promotions', promotionRouter);
 app.use('/leaders', leaderRouter);
 
